2516189f9eb12228a77da2651ac46526b91e2604
joao-zac <102718142+joao-zac@users.noreply.github.com>
2024-09-02 00:17:32 -0300

    :sparkles: feat: Componente users-display

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,15 +1,20 @@
 import { NgModule } from "@angular/core"
 import { angularMaterialModule } from "../angular-material/angular-material.module";
 import { FiltersComponent } from './filters/filters.component';
+import { UsersDisplayComponent } from './users-display/users-display.component';
 
 @NgModule({
     declarations: [
-      FiltersComponent
+      FiltersComponent,
+      UsersDisplayComponent
     ],
     imports: [
       angularMaterialModule
     ],
-    exports: [FiltersComponent]
+    exports: [
+      FiltersComponent,
+      UsersDisplayComponent
+    ]
 })
 export class ComponentsModule {
         
